128fdad62a1fe5a66a3c3ae1b9e52f1982decdd4
Jason Li <Jason.Li8885551@gmail.com>
2025-02-10 02:38:57 -0800

    Added a dictionary that would store colors and object name depending on what room you were in (1st floor only)

---
+++ Assets/Scripts/InLivingRoom.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InLivingRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+        gameObject.tag = "LivingRoom"; // Ensure the tag is set
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (RoomManager.Instance != null) // Check if GameManager exists
+        {
+            RoomManager.Instance.SetCurrentRoom(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public static RoomManager Instance { get; private set; }
+
+    // Define the event
+    public event Action<GameObject> OnRoomChanged;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetCurrentRoom(GameObject room)
+    {
+        //Debug.Log("Room set to: " + room.name);
+
+        // Trigger the event when the room is set
+        OnRoomChanged?.Invoke(room);
+    }
+}
