07fdfdb60791255cb7f3770cb4a63b89f0824a18
Jackie Huang <92900160+jhuan280@users.noreply.github.com>
2025-04-15 20:08:02 -0700

    Updated Kitchen level

---
+++ Assets/Scripts/destroyItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyItem : MonoBehaviour
+{
+
+    public GameObject itemToDestroy; // Assign the item to destroy in the inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // // gameObject.GetComponent<Renderer>().material == GameManager.Instance.grayMaterial
+        // if (itemToDestroy.GetComponent<Renderer>().material == GameManager.Instance.grayMaterial){
+
+        //     Destroy(itemToDestroy);
+        //     // Debug.Log("destroyed item");
+        // }
+        if (itemToDestroy.GetComponent<Renderer>().material.color == GameManager.Instance.grayMaterial.color)
+        {
+            Destroy(itemToDestroy);
+        }
+        
+    }
+}
