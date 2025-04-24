15992a40bfc65f6d70ef6dcaf7f4345cd5bcdb85
staypat <phhu@ucsc.edu>
2025-04-24 00:44:47 -0700

    Updated photos

--- Assets/Scripts/UI/PhotoManager.cs
+++ Assets/Scripts/UI/PhotoManager.cs
@@ -8,19 +8,25 @@ public class PhotoManager : MonoBehaviour
     private Dictionary<string, Image> photoCollection = new Dictionary<string, Image>();
 
     public Image hallwayPhoto;
-    public Image livingRoomPhoto;
+    public Image livingRoom1;
+    public Image livingRoom2;
     public Image kitchenPhoto1;
     public Image kitchenPhoto2;
     public Image kitchenPhoto3;
+    public Image MCbedroomPhoto1;
+    public Image MCbedroomPhoto2;
 
     // Start is called before the first frame update
     void Start()
     {
         photoCollection.Add("Hallway", hallwayPhoto);
-        photoCollection.Add("LivingRoom", livingRoomPhoto);
+        photoCollection.Add("LivingRoom1", livingRoom1);
+        photoCollection.Add("LivingRoom2", livingRoom2);
         photoCollection.Add("Kitchen1", kitchenPhoto1);
         photoCollection.Add("Kitchen2", kitchenPhoto2);
         photoCollection.Add("Kitchen3", kitchenPhoto3);
+        photoCollection.Add("MCBedroom1", MCbedroomPhoto1);
+        photoCollection.Add("MCBedroom2", MCbedroomPhoto2);
     }
 
     // Update is called once per frame
