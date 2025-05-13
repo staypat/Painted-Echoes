96016b8446e31ab404a17d6d635865f0cb43fc87
edmfong <97255519+edmfong@users.noreply.github.com>
2025-05-12 19:39:23 -0700

    added animations to golem and paintbrush

--- Assets/Scripts/Player/Click_2.cs
+++ Assets/Scripts/Player/Click_2.cs
@@ -63,6 +63,7 @@ public class Click_2 : MonoBehaviour
     private string currentAbsorbKeybind;
 
     // private bool hasPressedRightClickFirstTime = false; // Absorb color for tutorial text
+    public Animator animator;
 
     
     void Start()
@@ -478,6 +479,7 @@ public class Click_2 : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, maxDistance) && AmmoManager.Instance.GetCurrentAmmo(currentGunColor) > 0)
         {
+            animator.Play("paintbrushPaint");
             GameObject clickedObject = hit.collider.gameObject;
             Transform parent = clickedObject.transform.parent;
             Transform subparent = parent != null ? parent : null; // Get the subparent
@@ -707,6 +709,7 @@ public class Click_2 : MonoBehaviour
             // Apply absorbed color to brush
             gunRenderer.material = GetMaterialFromString(currentGunColor);
             brushTip.GetComponent<Renderer>().material = GetMaterialFromString(currentGunColor);
+            animator.Play("paintbrushAbsorb");
 
             // Increse ammo count for the absorbed color
             AmmoManager.Instance.AddAmmo(1, currentGunColor);
--- Assets/paintAnims.cs
+++ Assets/paintAnims.cs
@@ -13,9 +13,14 @@ public class paintAnims : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
+        if (Input.GetMouseButtonDown(0)) // Left click
         {
             StartPaint();
+        }
+
+        if (Input.GetMouseButtonDown(1)) // Right click
+        {
+            StartAbsorb();
         }
     }
 
@@ -24,6 +29,13 @@ public class paintAnims : MonoBehaviour
         animator.SetBool("paint", true);
         animator.SetBool("idle", false);
         StartCoroutine(WaitForPaintToFinish());
+    }
+
+    public void StartAbsorb()
+    {
+        animator.SetBool("absorb", true);
+        animator.SetBool("idle", false);
+        StartCoroutine(WaitForAbsorbToFinish());
     }
 
     private IEnumerator WaitForPaintToFinish()
@@ -35,6 +47,17 @@ public class paintAnims : MonoBehaviour
         );
 
         animator.SetBool("paint", false);
+        animator.SetBool("idle", true);
+    }
+    private IEnumerator WaitForAbsorbToFinish()
+    {
+        // Wait until the paint animation state exits
+        yield return new WaitUntil(() =>
+            animator.GetCurrentAnimatorStateInfo(0).IsName("paintbrushAbsorb") &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f
+        );
+
+        animator.SetBool("absorb", false);
         animator.SetBool("idle", true);
     }
 }
